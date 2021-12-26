bfc146d47d774f213a1d3cf0e58bc1f9843ee0d0
Mukul Singh <mukulsingh3344@gmail.com>
2021-12-26 18:41:10 +0530

    Add about section to struture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import About from "./components/about/About";
 import Intro from "./components/introduction/Intro";
 
 const App = () => {
     return (
         <div className="main">
             <Intro />
+            <About />
         </div>
     );
 };
---
+++ src/components/about/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./about.css";
+
+const About = () => {
+    return (
+        <div className="about">
+            <div className="about__left"></div>
+            <div className="about__right"></div>
+        </div>
+    );
+};
+
+export default About;
