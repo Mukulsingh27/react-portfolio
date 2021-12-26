c791e95e3accc95b4d4ee99a45de9d84f9bba573
Mukul Singh <mukulsingh3344@gmail.com>
2021-12-26 21:55:35 +0530

    Add title & desc with product card

---
+++ src/components/product/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = () => {
+    return (
+        <div className="pro__card">
+            <p>Card</p>
+        </div>
+    );
+};
+
+export default Product;
--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -1,10 +1,29 @@
 import React from "react";
+import Product from "./Product";
 import "./productList.css";
 
 const ProductList = () => {
     return (
         <div className="product">
-            <h1>Product</h1>
+            <div className="product_texts">
+                <h1 className="product__text__title">
+                    Create & Inspire. It's Lama
+                </h1>
+                <p className="product__text__desc">
+                    In the aad_process_ajax php function I have added all my
+                    code that loads the xml file with simplexml_load_file and
+                    loops trough it in a foreach loop, where I define all the
+                    data needs to be insertet in the database, and it checks if
+                    the products exists, updates them with new prices if they do
+                    etc etc.
+                </p>
+            </div>
+            <div className="product__list">
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+            </div>
         </div>
     );
 };
