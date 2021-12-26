70572be7aee225c1c27f3160ede40c586414f7b8
Mukul Singh <mukulsingh3344@gmail.com>
2021-12-26 21:35:56 +0530

    Add product section to structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "./components/about/About";
 import Intro from "./components/introduction/Intro";
+import ProductList from "./components/product/ProductList";
 
 const App = () => {
     return (
         <div className="main">
             <Intro />
             <About />
+            <ProductList />
         </div>
     );
 };
---
+++ src/components/product/ProductList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./productList.css";
+
+const ProductList = () => {
+    return (
+        <div className="product">
+            <h1>Product</h1>
+        </div>
+    );
+};
+
+export default ProductList;
